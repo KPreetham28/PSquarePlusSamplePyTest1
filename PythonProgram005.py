4de220eccb9803fc2f97346a5be7ba028345185b
Preetham Kompella <preetham.kompella@gmail.com>
2024-06-11 11:48:55 +0530

    PythonProgram005 is about operators.

---
+++ PythonProgram005.py
@@ -0,0 +1,98 @@
+'''
+Python Operators and their names with examples.
+Operator	Name		Example
++		Addition		x + y	
+-		Subtraction		x - y	
+*		Multiplication	x * y	
+/		Division		x / y	
+%		Modulus			x % y	
+**		Exponentiation	x ** y	
+//		Floor division	x // y
+'''
+
+'''
+Python assignment operators tricks
+Operator	Example		Same As	
+=			x = 5		x = 5	
++=			x += 3		x = x + 3	
+-=			x -= 3		x = x - 3	
+*=			x *= 3		x = x * 3	
+/=			x /= 3		x = x / 3	
+%=			x %= 3		x = x % 3	
+//=			x //= 3		x = x // 3	
+**=			x **= 3		x = x ** 3	
+&=			x &= 3		x = x & 3	
+|=			x |= 3		x = x | 3	
+^=			x ^= 3		x = x ^ 3	
+>>=			x >>= 3		x = x >> 3	
+<<=			x <<= 3		x = x << 3
+'''
+
+'''
+Python relational operators
+Operator	Name						Example
+==			Equal						x == y	
+!=			Not equal					x != y	
+>			Greater than				x > y	
+<			Less than					x < y	
+>=			Greater than or equal to	x >= y	
+<=			Less than or equal to		x <= y
+'''
+
+'''
+Python logical operators
+Operator	Description														Example
+and 		Returns True if both statements are true						x < 5 and  x < 10	
+or			Returns True if one of the statements is true					x < 5 or x < 4	
+not			Reverse the result, returns False if the result is true			not(x < 5 and x < 10)
+'''
+
+'''
+Python Identity operators
+Operator	Description													Example
+is 			Returns True if both variables are the same object			x is y	
+is not		Returns True if both variables are not the same object		x is not y
+'''
+
+'''
+Python Membership operators
+Operator	Description		
+in 			Returns True if a sequence with the specified value is present in the object	
+Example: x in y	
+not in		Returns True if a sequence with the specified value is not present in the object	
+Example: x not in y
+'''
+
+'''
+Operator	Name					Description															
+& 			AND						Sets each bit to 1 if both bits are 1	
+Example: x & y	
+|			OR						Sets each bit to 1 if one of two bits is 1	
+Example: x | y	
+^			XOR						Sets each bit to 1 if only one of two bits is 1	
+Example: x ^ y	
+~			NOT						Inverts all the bits			
+Example: ~x	
+<<   		Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	
+Example: x << 2	
+>>			Signed right shift		Shift right by pushing copies of the leftmost bit in from the left, 
+									and let the rightmost bits fall off	
+Example: x >> 2
+'''
+
+'''
+Operator										Description	Try it
+()												Parentheses	
+**												Exponentiation	
++x  -x  ~x										Unary plus, unary minus, and bitwise NOT	
+*  /  //  %										Multiplication, division, floor division, and modulus	
++  -											Addition and subtraction	
+<<  >>											Bitwise left and right shifts	
+&												Bitwise AND	
+^												Bitwise XOR	
+|												Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	Logical										NOT	
+and												AND	
+or												OR
+'''
